99000c5ec516f3e2ceb59a988e991560d523754f
KeerthanaReddy-15 <keerthanareddytholeti@gmail.com>
2026-03-29 10:02:27 +0530

    fixed navigate hook

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate} from 'react-router-dom';
 
 const Navbar = () => {
   const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+  const navigate =useNavigate();
 
   const logoutHandler = () => {
     localStorage.removeItem('userInfo');
-    window.location.href = '/login';
+    navigate('/login');
   };
 
   return (
